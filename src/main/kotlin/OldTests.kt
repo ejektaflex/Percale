cdeb90512d0931b76f7639f324a55365de4ec9df
E F <dootydooter@gmail.com>
2024-07-12 13:28:55 -0500

    Move to technically arbitrary ops

--- src/main/kotlin/OldTests.kt
+++ src/main/kotlin/OldTests.kt
@@ -3,8 +3,8 @@ import com.mojang.serialization.Codec
 import com.mojang.serialization.DataResult
 import com.mojang.serialization.DynamicOps
 import com.mojang.serialization.Encoder
+import com.mojang.serialization.JavaOps
 import com.mojang.serialization.JsonOps
-import com.mojang.serialization.codecs.PrimitiveCodec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import kotlinx.serialization.*
 import kotlinx.serialization.builtins.serializer
@@ -17,6 +17,8 @@ import kotlinx.serialization.encoding.Encoder as KEncoder
 import kotlinx.serialization.json.*
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.contextual
+import percale.encoder.PassEncoder
+import com.google.gson.JsonElement as GsonElement
 
 
 /*
@@ -40,89 +42,97 @@ fun <U : Any> KSerializer<U>.toCodec(): Codec<U> {
 }
  */
 
-class KotlinJsonOps(val json: Json.Default) : DynamicOps<JsonElement> {
-    override fun empty(): JsonElement {
-        return JsonNull
+// This is gross but ok
+object GsonElementSerializer : KSerializer<GsonElement> {
+    override val descriptor: SerialDescriptor
+        get() = JsonElement.serializer().descriptor
+    override fun serialize(encoder: KEncoder, value: GsonElement) {
+        encoder.encodeSerializableValue(JsonElement.serializer(), Json.decodeFromString(JsonElement.serializer(), value.toString()))
     }
 
-    override fun createNumeric(i: Number): JsonElement {
-        println("Encoding: $i")
-        return when (i) {
-            is Int -> json.encodeToJsonElement(Int.serializer(), i)
-            is Long -> json.encodeToJsonElement(Long.serializer(), i)
-            is Double -> json.encodeToJsonElement(Double.serializer(), i)
-            is Float -> json.encodeToJsonElement(Float.serializer(), i)
-            else -> throw Exception("Can't encode numeric here")
-        }
+    override fun deserialize(decoder: Decoder): GsonElement {
+        TODO("Not yet implemented")
+    }
+}
+
+class PassOps<A : Any>(val ops: DynamicOps<A>) : DynamicOps<A> {
+    override fun empty(): A {
+        return ops.empty()
     }
 
-    override fun createString(value: String): JsonElement {
-        return json.encodeToJsonElement(value)
-    }
-
-    override fun remove(input: JsonElement, key: String?): JsonElement {
+    override fun getNumberValue(input: A): DataResult<Number> {
         TODO("Not yet implemented")
     }
 
-    override fun createList(input: Stream<JsonElement>?): JsonElement {
+    override fun <U : Any?> convertTo(outOps: DynamicOps<U>?, input: A): U {
         TODO("Not yet implemented")
     }
 
-    override fun getStream(input: JsonElement): DataResult<Stream<JsonElement>> {
+    override fun createNumeric(i: Number): A {
+        val pEnc = PassEncoder.pickEncoder(PrimitiveSerialDescriptor("yes", PrimitiveKind.INT), ops)
+        pEnc.encodeSerializableValue(Int.serializer(), i.toInt())
+        return pEnc.getResult()
+    }
+
+    override fun createString(value: String): A {
+        val pEnc = PassEncoder.pickEncoder(PrimitiveSerialDescriptor("yess", PrimitiveKind.STRING), ops)
+        pEnc.encodeSerializableValue(String.serializer(), value)
+        return pEnc.getResult()
+    }
+
+    override fun remove(input: A, key: String?): A {
         TODO("Not yet implemented")
     }
 
-    override fun createMap(map: Stream<Pair<JsonElement, JsonElement>>?): JsonElement {
+    override fun createList(input: Stream<A>?): A {
         TODO("Not yet implemented")
     }
 
-    override fun getMapValues(input: JsonElement): DataResult<Stream<Pair<JsonElement, JsonElement>>> {
+    override fun getStream(input: A): DataResult<Stream<A>> {
         TODO("Not yet implemented")
     }
 
-    override fun mergeToMap(map: JsonElement, key: JsonElement, value: JsonElement): DataResult<JsonElement> {
-        return DataResult.success(
-            buildJsonObject {
-                (map as? JsonObject)?.let {
-                    // todo make recursive for deep copy
-                    for ((k, v) in it) put(k, v)
-                }
-                put(key.jsonPrimitive.content, value)
-            }
-        )
-    }
-
-    override fun mergeToList(list: JsonElement, value: JsonElement): DataResult<JsonElement> {
+    override fun createMap(map: Stream<Pair<A, A>>?): A {
         TODO("Not yet implemented")
     }
 
-    override fun getStringValue(input: JsonElement): DataResult<String> {
+    override fun getMapValues(input: A): DataResult<Stream<Pair<A, A>>> {
         TODO("Not yet implemented")
     }
 
-    override fun getNumberValue(input: JsonElement): DataResult<Number> {
+    /*
+    TODO in the case that value is a serializable type, we can perhaps pass a serialModule to the entire Ops and then
+    grab the serializer contextually ?? maybe this won't work but there might be something to it
+     */
+    override fun mergeToMap(map: A, key: A, value: A): DataResult<A> {
+        return ops.mergeToMap(map, key, value)
+    }
+
+    override fun mergeToList(list: A, value: A): DataResult<A> {
         TODO("Not yet implemented")
     }
 
-    override fun <U : Any?> convertTo(outOps: DynamicOps<U>?, input: JsonElement): U {
+    override fun getStringValue(input: A): DataResult<String> {
         TODO("Not yet implemented")
     }
-
 }
 
 
 
 
-fun <A> Encoder<A>.toKotlinJsonSerializer(ops: KotlinJsonOps = KotlinJsonOps(Json)): KSerializer<A> {
+
+fun <A> Encoder<A>.toKotlinJsonSerializer(): KSerializer<A> {
+    return toGenericSer(JsonOps.INSTANCE, GsonElementSerializer)
+}
+
+fun <A, U> Encoder<A>.toGenericSer(ops: DynamicOps<U>, opsSerializer: KSerializer<U>): KSerializer<A> {
     return object : KSerializer<A> {
         override val descriptor: SerialDescriptor
             get() = PrimitiveSerialDescriptor("test", PrimitiveKind.INT)
 
         override fun serialize(encoder: KEncoder, value: A) {
-            println("Auto-gen ser encoding..")
-            val result = this@toKotlinJsonSerializer.encodeStart(ops, value)
-            println("Result: ${result.result().get()}")
-            encoder.encodeSerializableValue(JsonElement.serializer(), result.result().get())
+            val result = this@toGenericSer.encodeStart(ops, value).result().get()
+            encoder.encodeSerializableValue(opsSerializer, result)
         }
 
         override fun deserialize(decoder: Decoder): A {
@@ -130,28 +140,6 @@ fun <A> Encoder<A>.toKotlinJsonSerializer(ops: KotlinJsonOps = KotlinJsonOps(Jso
         }
     }
 }
-
-
-/*
-
-We created a KSX Encoder/Decoder that wraps DynamicOps calls
-Now we need to create a DynamicOps that wraps KSX calls
-
-Then...
-
-OurNbtOps.INSTANCE.encodeStart(
-
-
-
-
-
-
-WHAT WE WANT:
-
-BlockPos.CODEC.toSerializer()
-
-
- */
 
 data class MyPerson(val name: String, val age: Int)
 
@@ -165,9 +153,10 @@ val MyPersonCodec : Codec<MyPerson> = RecordCodecBuilder.create { instance ->
 @Serializable
 data class MyParty(val size: Int, val organizer: @Contextual MyPerson)
 
+
+
 fun main() {
 
-    val kotlinOps = KotlinJsonOps(Json.Default)
 
     val ourJson = Json {
         serializersModule = SerializersModule {
@@ -179,7 +168,7 @@ fun main() {
     // as such, it's the same as a Kotlin KSerializer
 
     // KSerializer is our version of Codec
-    val ser = MyPersonCodec.toKotlinJsonSerializer(kotlinOps)
+    val ser = MyPersonCodec.toKotlinJsonSerializer()
 
     // Abstract[En/De]coder is our version of DynamicOps
     // Json is our version of JsonOps
@@ -194,11 +183,20 @@ fun main() {
     println(result)
 
 
-    val party = MyParty(33, MyPerson("Jimothy", 36))
 
-    val partyEncoded = ourJson.encodeToJsonElement(MyParty.serializer(), party)
+    val passOps = PassOps(JsonOps.INSTANCE)
+    val passSer = MyPersonCodec.toGenericSer(passOps, GsonElementSerializer)
 
-    println(partyEncoded)
+    val personPassEncoded = ourJson.encodeToString(passSer, MyPerson("Jimothy", 36))
+
+    println("PE:")
+    println(personPassEncoded)
+
+//    val party = MyParty(33, MyPerson("Jimothy", 36))
+//
+//    val partyEncoded = ourJson.encodeToJsonElement(MyParty.serializer(), party)
+//
+//    println(partyEncoded)
 
 
 }
